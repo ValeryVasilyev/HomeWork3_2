dcc650d36ba227c071cb24bfa48429e419c1c860
21200166 <VPavVasilyev@sberbank.ru>
2023-12-23 12:17:17 +0300

    The second Homework

---
+++ src/main/java/AddressBook.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class AddressBook {
+    private HashMap<String, String> addressBook = new HashMap<>();
+
+    /** Метод добавляет контакт в телефонный справочник */
+    public void add(String surname, String phoneNumber) {
+        if(addressBook.containsKey(surname)){
+            addressBook.put(surname, addressBook.get(surname) + ", " + phoneNumber);
+        }
+        else{
+            addressBook.put(surname, phoneNumber);
+        }
+    }
+
+    /** Метод выводит телефон контакта из телефонного справочника,
+     * в т.ч. если контакт встречается в справочнике несколько раз */
+    public void get(String surname) {
+        for (String contact : addressBook.keySet()) {
+            if (contact.equals(surname)) {
+                System.out.println("Contact: " + surname + " has phone number: " + addressBook.get(surname));
+             }
+        }
+
+    }
+}
--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,8 +1,21 @@
-public class Start {
+public class Start implements FirstPart {
     public static void main(String[] args) {
-        Child child1 = new Child();
+        System.out.println("The first part of Homework\n");
+        FirstPart.getUniqueValues(FirstPart.fillArray());
+        System.out.println("==============================");
+        FirstPart.countValues(FirstPart.fillArray());
 
-        Child child2 = new Child("Ivan");
+        System.out.println("\nThe second part of Homework\n");
+        AddressBook addressBook = new AddressBook();
+        addressBook.add("Pelevin", "+380991234567");
+        addressBook.add("Kolin", "+380991234567");
+        addressBook.add("Kolin", "+380991234561");
+        addressBook.add("Bulgarin", "+780991234567");
+        addressBook.add("Petrov", "+9999999");
+        addressBook.get("Kolin");
+        addressBook.get("Pelevin");
+        addressBook.get("Bulgarin");
+        addressBook.get("Petrov");
     }
 
 }
